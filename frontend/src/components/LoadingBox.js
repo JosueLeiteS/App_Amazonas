de234be982da59fde2f46b9298604044f7040d54
JosLeite <josueleitesan@gmail.com>
2022-04-15 21:45:04 -0500

    Loading and error message components

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,9 @@
+import Spinner from "react-bootstrap/Spinner";
+
+export default function LoadingBox() {
+  return (
+    <Spinner animation="border" roll="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+  );
+}
